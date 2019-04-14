677909c1161f942d0c94d34d13b8619f054c9fa0
dazer <dazer017@gmail.com>
2019-04-14 20:28:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mqtt-lightify',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/dzerrenner/mqtt-lightify',
+      author='David Zerrenner',
+      author_email='dazer017@gmail.com',
+      license='MIT',
+      packages=['mqtt-lightify'],
+      zip_safe=False)
